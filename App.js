f7e16ab762749cd8f25960f6f208c83a2b23b7bf
LayanaBhaskaran <81735644+LayanaBhaskaran@users.noreply.github.com>
2021-09-13 17:08:08 +0530

    Update App.js

--- App.js
+++ App.js
@@ -3,46 +3,35 @@ import './App.css';
 //import Body from './components/Body';
 
 //import Body from './components/Body';
-import Header from './components/Header';
+//import Header from './components/Header';
 import Registration from './components/Registration';
-import Sidebar from './components/Sidebar';
-import Login_form from './components/Login_form';
+//import Sidebar from './components/Sidebar';
+//import Login_form from './components/Login_form';
 
 import {BrowserRouter as Router,Route} from 'react-router-dom';
 import Fbheader from './components/Fbheader';
 import Side_Bar from './components/Side_Bar';
 import Feed from './components/Feed';
+import Widges from './components/Widges';
+import Login from './components/Login';
 
 function App() {
+  const user = null;
   return (
-    // <div >
-    //   <div>
-    //   {/* <Fbheader/> */}
-    //   <Header/>
-    //    {/* <Login_form/>    */}
-    //     {/* <Registration/>   */}
-    //  <Sidebar/>  
-    //  {/* <div><Body/></div> */}
-    //   </div>
-    // </div>
-
-     
-      // <Router>
-      //   <Header/>
-      //   <Route path="/" exact component={Sidebar}/>
-      //   <Route path="/Login_form" component={Login_form}/>
-      //   <Route path="/Registration" component={Registration}/>
-      //   </Router>
-
-
-
+  
 
       <div className="app"> 
-        <Fbheader/>
+      {user?(<Registration/>):
+      <>
+       <Fbheader/>
         <div className="app_body">
           <Side_Bar/>
           <Feed/>
+          <Widges/>
         </div>
+      </>
+      }
+       
       </div>
 
 
